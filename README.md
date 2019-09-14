07c7f4f9ffcab8da9decf1170a0a83b91c79980f
Pepe Valdivia <jvaldivia@softweb.pe>
2019-09-14 10:24:00 -0500

    Leyendo CSV con parseo

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 Fuentes:
 
 + https://www.rubyguides.com/2018/10/parse-csv-ruby/
++ https://snippets.aktagon.com/snippets/246-how-to-parse-csv-data-with-ruby
--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
 require 'csv'
 
-CSV.read('carrers.csv')
+CSV.foreach(
+  'data/carrers.csv',
+  quote_char: '"',
+  col_sep: '|',
+  row_sep: :auto,
+  headers: true,
+) do |row|
+  puts row[0]
+  puts row['id']
+  puts row['name']
+end
